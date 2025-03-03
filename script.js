05a844d2b1be2427a45302abf9cfb7e7ba754fc1
Jcrazi8 <joshuaramlagan0808@gmail.com>
2025-03-03 22:41:28 +0000

    updatesv2

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function showAlert() {
     alert("Hello world from Macbook i cannot wait to see how the world look one day when Ai takes over ");
 }
+
+const sentences = [
+    "Success is not final, failure is not fatal: it is the courage to continue that counts. – Winston Churchill",
+    "Believe you can, and you're halfway there. – Theodore Roosevelt",
+    "The only way to do great work is to love what you do. – Steve Jobs",
+    "Your only limit is your mind. Push past it, and anything is possible",
+    "Difficult roads often lead to beautiful destinations. Keep going."
+];
+
+const randomSentence = sentences[Math.floor(Math.random() * sentences.length)];
+
+console.log(randomSentence);
